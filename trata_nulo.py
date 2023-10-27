a61e255b06390a8cfec0135043cf175d829d94f9
FeGonca <felip.goncalves@hotmail.com>
2023-10-26 21:03:32 -0300

    alterando a funcionalidade trata nulo

--- trata_nulo.py
+++ trata_nulo.py
@@ -1,11 +1,11 @@
-list_path = dbutils.fs.ls('dbfs:/FileStore/data/')
-dbutils.fs.mkdirs('dbfs:/FileStore/data_tratados/')
+import os
+import pandas as pd
 
-for path in list_path:
-    df_name = path[1]
-    df = spark.read.csv(path[0], inferSchema=True, header=True)
+list_arquivos = os.listdir('./Data')
+
+for arquivo in list_arquivos:
+    df = pd.read_csv(f'./Data/{arquivo}')
     for coluna in df.columns:
-        df = df.withColumn(coluna, F.expr(f"replace({coluna}, '\\\\N', '')"))   
-    #df.write.csv(f'dbfs:/FileStore/data_tratados/{df_name}',header = True, sep = ';')
-    #df.write.option("header", "true").option("delimiter", ";").mode("overwrite").csv(f'dbfs:/FileStore/data_tratados/{df_name}')
-    df.write.mode("overwrite").format("csv").option("header", "true").option("delimiter", ";").save(f'dbfs:/FileStore/data_tratados/{df_name}')
+        df = df.replace(to_replace=r'^\\N$', value='', regex=True)
+    
+    df.to_csv(f'./Data_Tratada/{arquivo}', header=True, sep=';', index=False)
